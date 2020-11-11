9d80aaea8e41d2dbf0fe3aad75cb7c46dfd6f6ab
Chris Waitt <chris_waitt@me.com>
2020-11-11 14:15:14 +0000

    Handle errors more gracefully while running main().

--- all_checks.py
+++ all_checks.py
@@ -7,14 +7,14 @@ def check_reboot():
 	"""Returns True if the computer has a pending reboot."""
 	return os.path.exists("/run/reboot-required")
 
-def check_disk_full(disk, min_absolute, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
 	"""Returns true if there isn't enough disk space, False otherwise."""
 	du = shutil.disk_usage(disk)
 	# Calculate the percentage of free space
 	percent_free = 100 * du.free / du.total
 	#Calculate how many free gigabytes
 	gigabytes_free = du.free / 2**30
-	if percent_free < min_percent or gigabytes_free < min_absolute:
+	if percent_free < min_percent or gigabytes_free < min_gb:
 		return True
 	return False
 
@@ -22,7 +22,11 @@ def main():
 	if check_reboot():
 		pring("Pending Reboot.")
 		sys.exit(1)
+	if check_disk_full("/", 2, 10):
+		print("Disk full.")
+		sys.exit(1)
+
 	print("Everything ok.")
-	sys.exit(0)
+	sys.exit(1)
 
 main()
