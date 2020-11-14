642fe7211d09c32fa81518b0a307d95ee9b11776
Chris Waitt <chris_waitt@me.com>
2020-11-14 17:14:23 +0000

    Fixed problem with psutil calling cpu_percentage, changed to cpu_percent.

--- all_checks.py
+++ all_checks.py
@@ -28,7 +28,7 @@ def check_root_full():
 
 def check_cpu_constrained():
 	"""Returns True if the cpu is having too much usage, False otherwise."""
-	return psutil.cpu_percentage(1) > 75
+	return psutil.cpu_percent(1) > 75
 
 
 def check_no_network():
