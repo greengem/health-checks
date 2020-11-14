d4127eb3397220842d68ffadf4c82be9d8b92228
Chris Waitt <chris_waitt@me.com>
2020-11-14 17:03:10 +0000

    Add a simple cpu usage check

--- all_checks.py
+++ all_checks.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import sys
 import socket
+import psutil
 
 def check_reboot():
 	"""Returns True if the computer has a pending reboot."""
@@ -25,6 +26,11 @@ def check_root_full():
 	return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 
+def check_cpu_constrained():
+	"""Returns True if the cpu is having too much usage, False otherwise."""
+	return psutil.cpu_percentage(1) > 75
+
+
 def check_no_network():
 		"""Returns True if it fails to resolve Google's URL, False otherwise"""
 		try:
@@ -38,6 +44,7 @@ def main():
 	checks=[
 		(check_reboot, "Pending Reboot."),
 		(check_root_full, "Root partition full."),
+		(check_cpu_constrained, "CPU load too high."),
 		(check_no_network, "No working network."),
 	]
 	everything_ok=True
